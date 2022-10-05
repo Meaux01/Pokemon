02c6dd3873e1a60f58abf459242e7631c8e5e2bc
meaux21 <maurice.grimes21@gmail.com>
2022-10-05 18:27:50 -0400

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send`<h1>Welcome To the Pokemon App!! Hooray!!</h1>`
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log('Im running')
+})
